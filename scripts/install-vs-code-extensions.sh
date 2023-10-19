15b8772d1311d954f6245d21a2aba99faf7e1c4a
Yuehu Lin <dev@fntsr.tw>
2023-10-19 18:03:10 +0800

    chore(dx): improve maintenance of VS Code extension list

--- scripts/install-vs-code-extensions.sh
+++ scripts/install-vs-code-extensions.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-code --install-extension Vue.volar
-code --install-extension Nuxt.mdc
+# List of extensions to install
+extensions=(
+  # Vue
+  "Vue.volar"
+  "Nuxt.mdc"
+)
+
+# Check if the 'code' command is available
+if ! command -v code &> /dev/null; then
+  echo "Error: 'code' command not found. Please ensure that Visual Studio Code is installed and in the system PATH."
+  exit 1
+fi
+
+# Install the extensions
+for extension in "${extensions[@]}"; do
+  code --install-extension "$extension" --force
+  echo
+done
