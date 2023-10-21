8c1407c22b03971515e35e4bf36e6b00eee2fa11
Yuehu Lin <dev@fntsr.tw>
2023-10-22 04:42:25 +0800

    feat(style): enable Typography feature of unoCSS
    # Conflicts:
    #	pages/[...slug].vue

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,14 @@
 import {
   defineConfig,
   presetAttributify,
+  presetTypography,
   presetWind,
 } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetAttributify(),
+    presetTypography(),
     presetWind(),
   ],
 })
