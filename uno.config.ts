a34645778e66ef2cd3112be30653da2bb7a64757
Yuehu Lin <dev@fntsr.tw>
2023-10-21 22:39:07 +0800

    chore(style): enable attributify feature of unoCSS

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,12 @@
 import {
   defineConfig,
+  presetAttributify,
   presetWind,
 } from 'unocss'
 
 export default defineConfig({
   presets: [
+    presetAttributify(),
     presetWind(),
   ],
 })
